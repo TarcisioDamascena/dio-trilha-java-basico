87f386471c773668240d850a21764e2da69bd76e
DamasX <84998412+TarcisioDamascena@users.noreply.github.com>
2024-05-08 13:19:36 -0300

    Aula Java Documentação

---
+++ tipos-variaveis/src/Comentario.java
@@ -0,0 +1,26 @@
+public class Comentario {
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+    public static void main(String[] args) {
+        // Comentario em uma unica linha
+
+        /*
+         * comentario MultiLine
+         * Olá,
+         * Eu sou um comentario
+         * que posso ser mais detalhadod
+         * quando necessário
+         */
+
+        /*
+         * Um comentario,
+         * não possui a finalidade de amenizar um algoritmo não estruturado,
+         * conforme as convenções da linguagem
+         */
+    }
+}
